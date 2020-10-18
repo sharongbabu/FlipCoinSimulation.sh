a77fd4cef9e124fe3df1d95f615a598438aab273
“Enter <merin@gmail.com>
2020-10-19 00:36:32 +0530

    third commit

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -26,6 +26,18 @@ echo ${coin[@]}
 
 
 
+#! /bin/bash
+declare -A coin1
+
+coin1[random1]="hh"
+coin1[random2]="ht"
+coin1[random3]="th"
+coin1[random4]="tt"
+
+echo ${coin1[@]}
+echo | awk '{print 1/4  }'
+
+
 
 
 
