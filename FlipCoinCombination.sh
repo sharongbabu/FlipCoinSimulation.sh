5673ca57b19075501ec340d24a9c42d35fe18546
“Enter <merin@gmail.com>
2020-10-19 00:31:29 +0530

    second commit

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -8,6 +8,23 @@ else
 echo "tail"
 fi
 
+#! /bin/bash
+declare -A coin
+i=0
+while [ $i -le 2 ]
+do
+if [ $((RANDOM%2)) -eq 1 ]
+then
+ coin[head]="h"
+else
+coin[tail]="t"
+i=$(($i+ 1 ))
+fi
+done
+echo ${coin[@]}
+
+
+
 
 
 
